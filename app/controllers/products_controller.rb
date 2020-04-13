d3280f358e280a20f9641242494c9f656b3834e1
tom.debek@gmail.com <tom.debek@gmail.com>
2020-04-13 21:34:53 +0200

    Add Search feature

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -4,13 +4,21 @@ class ProductsController < ApplicationController
   before_action :find_product, only: [:show]
 
   def index
-    @products = Product.paginate(page: params[:page], per_page: 6)
+    #@products = Product.paginate(page: params[:page], per_page: 6)
+    search
   end
 
   def show; end
 
-  private
+  def search
+    @products = ::ProductsSearcher.new(params[:q]).perform
+    if @products.empty?
+      flash[:notice] = 'There is no product you are looking for, please try again'
+      redirect_back(fallback_location: products_path) unless @products.any?
+    end
+  end
 
+  private
   def find_product
     @product = Product.find(params[:id])
   end
