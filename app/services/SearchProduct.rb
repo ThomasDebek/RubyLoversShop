69eacff65dfd4e4b2b4a20c6c1619c3c8bcfb6a1
tom.debek@gmail.com <tom.debek@gmail.com>
2020-04-16 13:38:41 +0200

    Change on Pg_search

--- app/services/SearchProduct.rb
+++ app/services/SearchProduct.rb
@@ -5,7 +5,8 @@ class SearchProduct
   def call(phrase)
     # query = ApplicationRecord.sanitize_sql_array(['SELECT products.* FROM products WHERE products.name ILIKE ?', "%#{phrase}%"])
     #Product.find_by_sql(query)
-    Product.where("products.name ILIKE ?", "%#{phrase}%")
+    #Product.where("products.name ILIKE ?", "%#{phrase}%")
+    Product.search_by_name(phrase)
   end
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_11_154631) do
+ActiveRecord::Schema.define(version: 2020_04_16_113506) do
 
   # These are extensions that must be enabled in order to support this database
+  enable_extension "fuzzystrmatch"
+  enable_extension "pg_trgm"
   enable_extension "plpgsql"
+  enable_extension "unaccent"
 
   create_table "products", force: :cascade do |t|
     t.string "name"
