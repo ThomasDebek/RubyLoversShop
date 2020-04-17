7c5408def41e2ad97b1b4809650b35b8a509b263
tom.debek@gmail.com <tom.debek@gmail.com>
2020-04-17 22:52:44 +0200

    Remove the commented code

--- app/services/SearchProduct.rb
+++ app/services/SearchProduct.rb
@@ -2,9 +2,6 @@
 
 class SearchProduct
   def call(phrase)
-    # query = ApplicationRecord.sanitize_sql_array(['SELECT products.* FROM products WHERE products.name ILIKE ?', "%#{phrase}%"])
-    # Product.find_by_sql(query)
-    # Product.where("products.name ILIKE ?", "%#{phrase}%")
     Product.search_by_name(phrase)
   end
 end
