0c8badac8b365a15e559afb7a3fc64d5227e5412
tom.debek@gmail.com <tom.debek@gmail.com>
2020-04-18 16:20:56 +0200

    Create method that checks list of items already has products

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -4,7 +4,6 @@ class LineItemsController < ApplicationController
   before_action :set_cart, only: [:create]
   before_action :set_line_item, only: [:show, :edit, :update, :destroy]
 
-
   def index
     @line_items = LineItem.all
   end
@@ -16,14 +15,13 @@ class LineItemsController < ApplicationController
     @line_item = LineItem.new
   end
 
-
   def edit
   end
 
   def create
     product = Product.find(params[:product_id])
-    @line_item = @cart.line_items.build(product: product)
-
+    #@line_item = @cart.line_items.build(product: product)
+    @line_item = @cart.add_product(product)
     if @line_item.save
       redirect_to @line_item.cart, notice: 'Line item was successfully created.'
     else
@@ -45,11 +43,13 @@ class LineItemsController < ApplicationController
   end
 
   private
-    def set_line_item
-      @line_item = LineItem.find(params[:id])
-    end
 
-    def line_item_params
-      params.require(:line_item).permit(:product_id, :cart_id)
-    end
+  def set_line_item
+    @line_item = LineItem.find(params[:id])
+  end
+
+  def line_item_params
+    params.require(:line_item).permit(:product_id, :cart_id)
+  end
+
 end
