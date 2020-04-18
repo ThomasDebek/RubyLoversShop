887a82b809ecc5c1aea2b39fb8e356b1dc0b4bc1
tom.debek@gmail.com <tom.debek@gmail.com>
2020-04-18 15:34:22 +0200

    Set Current cart, set controller product crate and set views

--- app/controllers/concerns/current_cart.rb
+++ app/controllers/concerns/current_cart.rb
@@ -6,7 +6,8 @@ module CurrentCart
   def set_cart
     @cart = Cart.find(session[:cart_id])
   rescue ActiveRecord::RecordNotFound
-    @cart = Cart.create session[:cart_id] = @cart.id
+    @cart = Cart.create
+    session[:cart_id] = @cart.id
   end
 
 end
