b869c771d5fcd98abbb69538c2be157197d0ed68
tom.debek@gmail.com <tom.debek@gmail.com>
2020-04-21 15:07:03 +0200

    fix

--- app/models/product.rb
+++ app/models/product.rb
@@ -9,7 +9,6 @@ class Product < ApplicationRecord
   validates :name, presence: true
   validates :description, presence: true
   validates :price, numericality: { greater_than_or_equal_to: 0.01 }
-  
 
   pg_search_scope :search_by_name, against: :name, using: { dmetaphone: {}, trigram: {}, tsearch: { prefix: true, any_word: true } }
 
