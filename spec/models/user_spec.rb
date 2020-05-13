6e91aaffbb1abdae357da11b399d6335996973f9
tom.debek@gmail.com <tom.debek@gmail.com>
2020-05-13 22:46:40 +0200

    Corrected test and deleting files .idea

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,26 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe User do
+    it "has a valid factory" do
+      expect(FactoryBot.build(:user)).to be_valid
+    end
+
+    it "is valid with email, and password" do
+      user = FactoryBot.build(:user)
+      expect(user).to be_valid
+    end
+
+    it "is invalid without an email address" do
+      user = User.new(email: nil)
+      user.valid?
+      expect(user.errors[:email]).to include("can't be blank")
+    end
+
+    it "does something with multiple users" do
+      user1 = FactoryBot.create(:user)
+      user2 = FactoryBot.create(:user)
+      expect(true).to be_truthy
+    end
+  end
 end
